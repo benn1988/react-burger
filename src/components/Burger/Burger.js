2d52cd404aa58bbf02865956a8974287edd042e8
benn1988 <36059686+benn1988@users.noreply.github.com>
2020-09-24 18:12:01 -0700

    Outputing burger ingredients dynamically
    Adding state to the Burger Builder
    Getting the object properties from state, and outputing the respective ingredients as many times as their values

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -3,11 +3,20 @@ import classes from './Burger.module.css';
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 
 const burger = ( props ) => {
+
+    const transformedIngredients = Object.keys(props.ingredients) // return an array of the ingredients properties
+        .map(igKey => { // for each ingredient property
+            return [...Array(props.ingredients[igKey])] // return an array containing the property X times (where X is the value of the property)
+                .map((_, index) => { // for each time the property appears
+                    return <BurgerIngredient key={igKey + index} type={igKey} /> // return the ingredient component with it's respective type
+                });
+        });
+
     return (
         <div className={classes.Burger}>
+        {/* top and bottom buns are always present */}
             <BurgerIngredient type="bread-top" />
-            <BurgerIngredient type="cheese" />
-            <BurgerIngredient type="meat" />
+            {transformedIngredients}
             <BurgerIngredient type="bread-bottom" />
         </div>
     );
