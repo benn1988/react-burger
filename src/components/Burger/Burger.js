230fe0b66d53d03fd8421356f0d353451ddbfbcb
benn1988 <36059686+benn1988@users.noreply.github.com>
2020-09-24 18:38:47 -0700

    Calculating the ingredient sum dynamically
    Concat all the possible arrays from transformedIngredients into 1 array, so we can check it's length. Use the reduce method to concatenate each item to the inital empty array.
    If there are no ingredients, show the message to add ingredients

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -4,13 +4,20 @@ import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 
 const burger = ( props ) => {
 
-    const transformedIngredients = Object.keys(props.ingredients) // return an array of the ingredients properties
+    let transformedIngredients = Object.keys(props.ingredients) // return an array of the ingredients properties
         .map(igKey => { // for each ingredient property
             return [...Array(props.ingredients[igKey])] // return an array containing the property X times (where X is the value of the property)
                 .map((_, index) => { // for each time the property appears
                     return <BurgerIngredient key={igKey + index} type={igKey} /> // return the ingredient component with it's respective type
                 });
-        });
+        }).reduce((arr , el) => { // concat all the possible arrays into 1 array, so we can check it's length. Use the reduce method to concatenate each item to the inital empty array
+            return arr.concat(el);
+        }, []);
+        
+    // if there are no ingredients (in the state), output this message
+    if (transformedIngredients.length === 0) {
+        transformedIngredients = <p>Please start adding ingredients!</p>
+    }
 
     return (
         <div className={classes.Burger}>
