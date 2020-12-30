55ad28c287b023b905207fd3a1841a1da580a2c2
benn1988 <36059686+benn1988@users.noreply.github.com>
2020-12-30 00:52:05 -0800

    231. Outputing the orders
    iterate over the response we get, and save the data in an array by using the spread method

--- src/containers/Orders/Orders.js
+++ src/containers/Orders/Orders.js
@@ -1,8 +1,35 @@
 import React, { Component } from 'react';
 
 import Order from '../../components/Order/Order';
+import axios from '../../axios-orders';
+import withErrorHandler from '../../hoc/withErrorHandler/withErrorHandler';
 
-export default class Orders extends Component {
+class Orders extends Component {
+    state = {
+        orders: [],
+        loading: true
+    }
+
+    componentDidMount() {
+        axios.get('/orders.json')
+            .then( res => {
+                const ordersFetched = [];
+                // iterate over the response we get, and save the data in an array by using the spread method
+                // save the order id as well
+                for (let key in res.data) {
+                    ordersFetched.push({
+                        ...res.data[key],
+                        id: key
+                    })
+                }
+                this.setState({loading: false, orders: ordersFetched});
+                console.log(ordersFetched);
+            })
+            .catch( err => {
+                this.setState({loading: false});
+            })
+    }
+
     render() {
         return (
             <div>
@@ -12,3 +39,5 @@ export default class Orders extends Component {
         )
     }
 }
+
+export default withErrorHandler(Orders, axios);
